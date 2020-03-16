1245defd8e3f01153cfde7a352c0fd03743ce5a8
amor.edlouise <amor.edlouise@gmail.com>
2020-03-16 22:19:21 +0800

    Updates

---
+++ precache-manifest.da971389121b06f245e32c9365d81eb2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41d213fc6afe7fa1fb344d77dd3ea89b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "58bb742b0b80dad665dc",
+    "url": "/portfolio/static/css/main.45395ff5.chunk.css"
+  },
+  {
+    "revision": "6d97786e85429b6eac15",
+    "url": "/portfolio/static/js/2.1df0f2c4.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/portfolio/static/js/2.1df0f2c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58bb742b0b80dad665dc",
+    "url": "/portfolio/static/js/main.7ad2d2e7.chunk.js"
+  },
+  {
+    "revision": "9d741e501c1183fc02ac",
+    "url": "/portfolio/static/js/runtime-main.be6bec99.js"
+  },
+  {
+    "revision": "03c1ce6651c3f0bb7bda89133cbfaec4",
+    "url": "/portfolio/static/media/CS1-thumbnail.03c1ce66.png"
+  },
+  {
+    "revision": "2ec7c1dd31c25e69e6d523e0a1264d72",
+    "url": "/portfolio/static/media/OnlineStore-thumbnail.2ec7c1dd.png"
+  },
+  {
+    "revision": "a7c7f4799456736f441ebe1fb85353e2",
+    "url": "/portfolio/static/media/css.a7c7f479.svg"
+  },
+  {
+    "revision": "a91ec9c4cfbbd12591964870f800c5af",
+    "url": "/portfolio/static/media/html.a91ec9c4.svg"
+  },
+  {
+    "revision": "a602fe1fb0c1cd24329e977873c1cc74",
+    "url": "/portfolio/static/media/java.a602fe1f.svg"
+  },
+  {
+    "revision": "de79c8f14c2b0d692ae215d7e6eccd54",
+    "url": "/portfolio/static/media/javascript.de79c8f1.svg"
+  },
+  {
+    "revision": "322a83062de5ae353ab2ca1bf9f831b7",
+    "url": "/portfolio/static/media/me.322a8306.png"
+  }
+]);
