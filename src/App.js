fabfb0ffd1ecf5c1c3d5e370df3a771866caa1a4
amor.edlouise <amor.edlouise@gmail.com>
2020-07-13 16:17:51 +0800

    fix redirect to version 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 // import Home from './components/Home/Index';
-import Redirect from './components/Redirect/Redirect';
+// import Redirect from './components/Redirect/Redirect';
 
 
 
 
 function App() {
 
-
+  useEffect(() => {
+    let url = "https://edamor.github.io/portfolio2";
+    window.location.replace(url);
+  })
 
   return (
     <BrowserRouter basename={process.env.PUBLIC_URL}>
         {/* <Route path="/" component={Home} /> */}
-        <Route path="/" component={Redirect} />
+        <Route path="/" render={() => (<div>...</div>)} />
     </BrowserRouter>
     
   );
